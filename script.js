a530f34ae567289dfeb31c309b4f1d95f64232ca
MARUDHU B <marudhub81@gmail.com>
2025-08-02 15:33:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
         { "name": "Archana S", "github": "ArchanaSenthilkumar06", "linkedin": "https://www.linkedin.com/in/archana-s-3403a9334?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app", "role": "Core Team" },
         { "name": "Monika Aruldoss", "github": "monikaaruldoss", "linkedin": "https://www.linkedin.com/in/monika-a-b54869291/", "role": "Core Team" },
         { "name": "Lavanya R", "github": "lavanyarajalingam", "linkedin": "https://www.linkedin.com/in/lavanya-r-070868291/", "role": "Core Team" },
-        { "name": "Jayanitha S", "github": "JayanithaSivakumar", "linkedin": "https://www.linkedin.com/in/jayanitha-s-023a43291/", "role": "Core Team" }
+        { "name": "Jayanitha S", "github": "JayanithaSivakumar", "linkedin": "https://www.linkedin.com/in/sjayanitha26?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app", "role": "Core Team" }
     ];
 
     // --- DYNAMIC TEAM HEXAGON INJECTION ---
